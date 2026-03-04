afc3f1d43f57f445f8d073b0e72aed5e6145cc0b
luisg0c <lgustavo5555@gmail.com>
2026-03-04 05:28:00 -0300

    implementa vocabulario e embeddings

--- encoder.py
+++ encoder.py
@@ -6,3 +6,18 @@ import pandas as pd
 D_MODEL = 64
 D_FF = 256
 N_CAMADAS = 6
+
+np.random.seed(7)
+
+vocab = pd.DataFrame({
+    "palavra": ["cada", "neural", "a", "frase", "rede", "processa", "da", "palavra"],
+    "id": range(8)
+})
+
+frase = "a rede neural processa cada palavra da frase"
+tokens = frase.split()
+ids = [int(vocab[vocab["palavra"] == t]["id"].values[0]) for t in tokens]
+
+embedding_table = np.random.randn(len(vocab), D_MODEL)
+X = embedding_table[ids]
+X = X.reshape(1, len(ids), D_MODEL)
